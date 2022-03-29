7d73d0ac53a5bd80a3bc80154be2210c5aad48eb
Antoni Kucharczyk <hello@antoni.work>
2022-03-29 11:37:21 +0200

    Add tailwind Add styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div  className="bg-yellow-400 h-screen"><Component {...pageProps} /></div>
 }
 
 export default MyApp
